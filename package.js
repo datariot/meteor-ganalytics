d9ea91a715b526809d34d5bd611246d00bf91416
David W. Allen <david@datariot.io>
2018-10-07 21:10:25 -0600

    Bump Version for adding AnonIP MR.
    Bumping the version for adding the functionality to respect anon from MR #28.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Publish pageviews and events to Google Analytics using the new analytics.js code.",
-  version: "0.2.1",
+  version: "0.3.0",
   name: "datariot:ganalytics", 
   git: 'https://github.com/datariot/meteor-ganalytics.git'
 });
